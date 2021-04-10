706ed18777151ec788b7299bebe3f244e7d6c044
Taha Ansari <taha.ansari@schbang.in>
2021-04-10 18:48:54 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <AppHeader></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
